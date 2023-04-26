5b03e592e71e1c4bc4c3eca2b0ab506929ac0f1b
Mahmoud Sayed <mahmoudsayed002@gmail.com>
2023-04-26 15:14:44 +0200

    updated

--- DTOs/CartProductDto.cs
+++ DTOs/CartProductDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace WebApi_app.DTOs
 {
-    public class CartProductDto
-    {
-        public int userId { get; set; }
-        public int productId { get; set; }
-        public int quantity { get; set; } 
-    }
+    public record CartProductDto
+    (
+         int userId,
+         int productId,
+         int quantity
+    );
 }
